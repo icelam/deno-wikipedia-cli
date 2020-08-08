21cc0aa5ff8f8eca1acb2499697d44d497ac225c
Ice Lam <pinky.yy.lam@gmail.com>
2020-08-08 23:19:51 +0800

    chore: bump deno version to 1.2.2 and std version to 0.63.0 (#6)

--- src/deps.ts
+++ src/deps.ts
@@ -1,14 +1,14 @@
 // TODO: Convert it to import map for easier management
 
 /* Dependencies */
-export { parse, Args } from 'https://deno.land/std@0.62.0/flags/mod.ts';
-export { readLines } from 'https://deno.land/std@0.62.0/io/bufio.ts';
-export * from 'https://deno.land/std@0.62.0/fmt/colors.ts';
-export { existsSync } from 'https://deno.land/std@0.62.0/fs/exists.ts';
-export { readJsonSync } from 'https://deno.land/std@0.62.0/fs/read_json.ts';
-export { writeJsonSync } from 'https://deno.land/std@0.62.0/fs/write_json.ts';
+export { parse, Args } from 'https://deno.land/std@0.63.0/flags/mod.ts';
+export { readLines } from 'https://deno.land/std@0.63.0/io/bufio.ts';
+export * from 'https://deno.land/std@0.63.0/fmt/colors.ts';
+export { existsSync } from 'https://deno.land/std@0.63.0/fs/exists.ts';
+export { readJsonSync } from 'https://deno.land/std@0.63.0/fs/read_json.ts';
+export { writeJsonSync } from 'https://deno.land/std@0.63.0/fs/write_json.ts';
 export { encodeUrl } from 'https://deno.land/x/encodeurl@1.0.0/mod.ts';
-export { default as AsciiTable } from 'https://deno.land/x/ascii_table@20dd719/mod.ts';
+export { default as AsciiTable } from 'https://deno.land/x/ascii_table@v0.1.0/mod.ts';
 
 /* Test Dependencies */
-export { Rhum } from 'https://deno.land/x/rhum@6953331/mod.ts';
+export { Rhum } from 'https://deno.land/x/rhum@v1.1.2/mod.ts';
